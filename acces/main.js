e3852324fd1554353294ab98692e2ef8953aeff7
Nguyen Xuan An <anxuan1234@gmail.com>
2023-06-11 17:35:38 +0700

    5

--- acces/main.js
+++ acces/main.js
@@ -1,1 +1,5 @@
-
+var headerbot__listbooks = document.querySelector(".header-bot__listbooks");
+var listbooks__list = document.querySelector(".listbooks-list");
+headerbot__listbooks.onclick = function(){
+    listbooks__list.classList.toggle("listbooks_none");
+}
