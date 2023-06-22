a98df4ad3649b5cab916ff0b9a95437a869177aa
XuanAn14 <131387743+XuanAn14@users.noreply.github.com>
2023-06-22 21:04:56 +0700

    Final

--- acces/chitiet.js
+++ acces/chitiet.js
@@ -1,4 +1,3 @@
-
 // fixed
 window.onscroll = function() {myFunction()};
 
@@ -6,6 +5,15 @@ var header = document.querySelector(".header-mid");
 var stickyHeader = header.offsetTop;
 var tab_detail = document.querySelector(".tab-detail");
 var stickyTab = tab_detail.offsetTop;
+
+
+
+var tab_info = document.querySelector(".information__title");
+var infoTab = tab_info.offsetTop;
+var tabPrevew = document.getElementById("tabReview");
+var tabInfo = document.getElementById("tabInfo");
+var tab_rate = document.querySelector(".comment__title");
+var infoRate = tab_rate.offsetTop;
 
 function myFunction() {
   if (window.scrollY > stickyHeader) {
@@ -17,11 +25,25 @@ function myFunction() {
   
   if (window.scrollY > stickyTab) {
     header.style.setProperty("display","none");
+    tabPrevew.classList.add("tab-detail__item--active");
     tab_detail.classList.add("tab-detail_fixed");
   } else {
     tab_detail.classList.remove("tab-detail_fixed");
   }
+  
+  if (window.scrollY > infoTab) {
+    tabPrevew.classList.remove("tab-detail__item--active");
+    tabInfo.classList.add("tab-detail__item--active");
+  } else {
+    tabInfo.classList.remove("tab-detail__item--active");
+  }
 
+  if (window.scrollY > infoRate) {
+    tabInfo.classList.remove("tab-detail__item--active");
+    tabRate.classList.add("tab-detail__item--active");
+  } else {
+    tabRate.classList.remove("tab-detail__item--active");
+  }
 }
 
 
